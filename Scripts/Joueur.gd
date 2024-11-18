d37d1b7c81453402131d2be7dc70ff20230a1a90
Philippe Mourey <philippe@mourey.com>
2024-11-18 14:50:54 +0100

    Initialisation des scènes

--- Scripts/Joueur.gd
+++ Scripts/Joueur.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
+const ACCEL = 0.5 * SPEED
+var spd : Vector2 = Vector2(0, 0)
 const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -10,8 +12,7 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
 	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
+	
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -19,10 +20,19 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	var direction_x = Input.get_axis("ui_left", "ui_right")
+	var direction_y = Input.get_axis("ui_up", "ui_down")
+	spd.x = max(min(SPEED, spd.x+direction_x*ACCEL),-SPEED)
+	spd.y = max(min(SPEED, spd.y+direction_y*ACCEL),-SPEED)
+	if direction_y == 0:
+		spd.y -= ACCEL / 2 if spd.y > 0 else -ACCEL / 2
+		if abs(spd.y) < 2 * ACCEL:
+			spd.y = 0
+	if direction_x == 0:
+		spd.x -= ACCEL / 2 if spd.x > 0 else -ACCEL / 2
+		if abs(spd.x) < 2 * ACCEL:
+			spd.x = 0
+
+	velocity = spd
 
 	move_and_slide()
