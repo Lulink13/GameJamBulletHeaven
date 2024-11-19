3ec8158d849005fbea459dc4f09293b3947ec1ee
Lulink13 <lefevre.lulu@gmail.com>
2024-11-19 11:58:16 +0100

    Spawner + fix collisions et dégâts

--- Scripts/Joueur.gd
+++ Scripts/Joueur.gd
@@ -33,22 +33,16 @@ func _physics_process(delta):
 	velocity = spd*delta
 	move_and_slide()
 
-#-- Prendre des dégâts --
+func damage(ammount:int):
 	if not invuln :
-		for i in get_slide_collision_count():
-			var collision = get_slide_collision(i)
-			if collision.get_collider() is Ennemi:
-				print("ennemi!!!")
-				hp -= collision.get_collider().get_contact_damage()
-				if hp <= 0:
-					$Sprite2D.queue_free()
-					$CollisionShape2D.queue_free()
-					alive = false
-				else : invuln = true
-				$InvulnTimer.start() 
-				modulate = Color("Red")
-				break
-	get_slide_collision_count()
+		hp -= ammount
+		if hp <= 0:
+			$Sprite2D.queue_free()
+			$CollisionShape2D.queue_free()
+			alive = false
+		else : invuln = true
+		$InvulnTimer.start() 
+		modulate = Color("Red")
 
 func _on_invuln_timer_timeout() -> void:
 	invuln = false
