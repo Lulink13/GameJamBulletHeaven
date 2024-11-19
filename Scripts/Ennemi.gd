68f7a11c8336d7470d9b51c3b73cf3b68154194d
Lulink13 <lefevre.lulu@gmail.com>
2024-11-19 09:08:24 +0100

    Cleaned-up and improved character movement

--- Scripts/Ennemi.gd
+++ Scripts/Ennemi.gd
@@ -1,9 +1,9 @@
-extends CharacterBody2D
+extends PhysicsBody2D
 class_name Ennemi
 
 var player: Node2D
-var direction: Vector2 = Vector2(0, 0)
-const SPEED = 3000.0
+#var direction: Vector2 = Vector2(0, 0)
+const SPEED = 140
 var ContactDamage = 1
 
 # Called when the node enters the scene tree for the first time.
@@ -12,13 +12,14 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	direction = player.position - position 
-	# velocity = direction.normalized() * SPEED * direction
-	velocity = direction.normalized() * SPEED * delta
-	# velocity = player.position.direction_to(position)
-	print_debug(direction)
+	var spd : Vector2
+	if player.alive :#and not player.invuln : 
+		spd = (player.position - position).normalized() * SPEED * delta
+	else : spd = Vector2(0,0)
 	
-	move_and_slide()
+	var collision = move_and_collide(spd)
+	#if collision.get_collider() is Joueur :
+		#print("joueur!!")
 
 func get_contact_damage():
 	return ContactDamage
--- Scripts/Joueur.gd
+++ Scripts/Joueur.gd
@@ -1,30 +1,38 @@
 extends CharacterBody2D
 class_name Joueur
 
-const SPEED : float = 30000.0
-const ACCEL : float = 0.1 * SPEED
+const SPEED : float = 40000
+const ACCEL : float = 240000
+const DECEL : float = 140000
 
 var hp : int = 10
 var spd : Vector2 = Vector2(0, 0)
 var invuln : bool = false
+var alive = true
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
-	var direction_x = Input.get_axis("ui_left", "ui_right")
-	var direction_y = Input.get_axis("ui_up", "ui_down")
-	spd.x = max(min(SPEED, spd.x+direction_x*ACCEL),-SPEED)
-	spd.y = max(min(SPEED, spd.y+direction_y*ACCEL),-SPEED)
-	if direction_y == 0:
-		spd.y -= ACCEL / 2 if spd.y > 0 else -ACCEL / 2
-		if abs(spd.y) < 2 * ACCEL:
-			spd.y = 0
-	if direction_x == 0:
-		spd.x -= ACCEL / 2 if spd.x > 0 else -ACCEL / 2
-		if abs(spd.x) < 2 * ACCEL:
-			spd.x = 0
+	var direction = Vector2(Input.get_axis("ui_left", "ui_right"), Input.get_axis("ui_up", "ui_down")).normalized()
+	#print(direction)
+	var tmp_spd : Vector2
+	
+	if direction.x == 0 :
+		tmp_spd.x = spd.move_toward(Vector2(0,0), DECEL*delta).x
+	#elif sign(direction.x) != sign(spd.x) :
+		#tmp_spd.x = spd.x + (direction.x * DECEL*delta)
+	else :	
+		tmp_spd.x = spd.x + (direction.x * ACCEL*delta)
+		
+	if direction.y == 0 :
+		tmp_spd.y = spd.move_toward(Vector2(0,0), DECEL*delta).y
+	#elif sign(direction.y) != sign(spd.y) :
+		#tmp_spd.y = spd.y + (direction.y * DECEL*delta)
+	else :
+		tmp_spd.y = spd.y + (direction.y * ACCEL*delta)
+	spd = tmp_spd.limit_length(SPEED)
 
 	velocity = spd*delta
 	move_and_slide()
@@ -33,14 +41,20 @@ func _physics_process(delta):
 			var collision = get_slide_collision(i)
 			if collision.get_collider() is Ennemi:
 				print("ennemi!!!")
-				hp -= collision.get_contact_damage()
-				if hp <= 0: queue_free()
-				else : invuln = false
-		$InvulnTimer.start()
+				hp -= collision.get_collider().get_contact_damage()
+				if hp <= 0:
+					$Sprite2D.queue_free()
+					$CollisionShape2D.queue_free()
+					alive = false
+				else : invuln = true
+				$InvulnTimer.start() 
+				modulate = Color("Red")
+				break
 	get_slide_collision_count()
 
 func _on_invuln_timer_timeout() -> void:
 	invuln = false
+	modulate = Color("White")
 
 func _on_attack_timer_timeout() -> void:
 	pass # Replace with function body.
