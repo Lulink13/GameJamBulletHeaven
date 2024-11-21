dff00cc169d27d61365ecb4fab4dc1a886287d18
Philippe Mourey <philippe@mourey.com>
2024-11-21 14:16:13 +0100

    update menus

--- Scripts/Menus/SelectCharacter.gd
+++ Scripts/Menus/SelectCharacter.gd
@@ -11,14 +11,14 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_dark_elf_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Niveau_test.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Niveaux/Niveau_UN.tscn")
 	
 func _on_fighter_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Niveau_test.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Niveaux/Niveau_UN.tscn")
 
 
 func _on_knight_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Niveau_test.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Niveaux/Niveau_UN.tscn")
 
 
 #func _on_elf_button_pressed() -> void:
